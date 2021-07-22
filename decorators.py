77abb25b5b7975e0706594a1e32810d6f47ed554
caiyin0211 <caiyin0211@gmail.com>
2021-07-22 09:42:35 +0900

    7/22 commit1

--- decorators.py
+++ decorators.py
@@ -14,4 +14,25 @@ def hello_world():
 hello_world()
 
 
+def check_integer(func):
+    def decorated(width, height):
+        if width >= 0 and height >= 0:
+            return func(width, height)
+        else:
+            raise ValueError('Input must be positive value')
+    return decorated
 
+
+@check_integer
+def rect_area(width, height):
+    return width * height
+
+@check_integer
+def tri_area(width, height):
+    return width * height/2
+
+r_area = rect_area(-10, 10)
+print(r_area)
+
+t_area = tri_area(10, 10)
+print(t_area)
